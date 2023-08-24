4b8507595050fc3c9f1d27f445981300ac3d96fb
requaid <66545324+requaid@users.noreply.github.com>
2023-08-25 01:52:06 +0900

    음악 재생 수정

--- makeFlowerWreath/src/index.js
+++ makeFlowerWreath/src/index.js
@@ -1,11 +1,24 @@
 var bgm = new Audio("./src/sound/bgm.mp3");
+var AudioContext;
+var audioContext;
 bgmPlay();
+// window.onload = function () {
+// 	navigator.mediaDevices.getUserMedia({ audio: true }).then(() => {
+// 		AudioContext = window.AudioContext || window.webkitAudioContext;
+// 		audioContext = new AudioContext();
+// 	}).catch(e => {
+// 		console.error(`Audio permissions denied: ${e}`);
+// 	});
+// }
+
 // $(".draggable").draggable(); // 오브젝트 드래그 속성
 
 //bgm 재생
 function bgmPlay() {
 	bgm.loop = true;
-	bgm.play();
+	bgm.muted = true;
+	bgm.oncanplaythrough = bgm.play();
+	bgm.muted = false;
 }
 function toggleBgm() {
 	var soundButton = document.getElementById('speaker');
